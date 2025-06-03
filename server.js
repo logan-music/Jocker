e0df7a1d82d7f87b756fa72034457eca156be1dc
logan-music <nasrylogan@gmail.com>
2025-06-03 10:27:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const axios = require("axios");
-const axiosCookieJarSupport = require("axios-cookiejar-support").default;
-const tough = require("tough-cookie");
-const fs = require("fs");
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-// Load cookies from cookies.json
-const cookies = require("./cookies.json");
-const jar = new tough.CookieJar();
+// Load cookies from file
+const cookies = require('./cookies.json');
 
 (async () => {
-  for (const cookie of cookies) {
-    await jar.setCookie(`${cookie.name}=${cookie.value}`, "https://www.betpawa.co.tz");
-  }
+  const browser = await puppeteer.launch({
+    headless: false, // kwa test, tunataka tuone kila kitu
+    args: ['--start-maximized']
+  });
 
-  const client = axios.create({ jar });
-  axiosCookieJarSupport(client);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 });
 
-  const res = await client.get("https://www.betpawa.co.tz");
-  console.log("Page loaded, status:", res.status);
+  // Apply cookies
+  await page.setCookie(...cookies.cookies);
+
+  // Open BetPawa homepage
+  await page.goto('https://www.betpawa.co.tz', { waitUntil: 'networkidle2' });
+
+  // Angalia kama imeingia vizuri (unaweza kubadili selector)
+  await page.waitForTimeout(5000);
+
+  // For now, just stay there
 })();
